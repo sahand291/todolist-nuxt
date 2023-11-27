b561caf970f2a243f80b1b598641519d38236f22
Sahand Karimkhani <sahandkarimkhani@gmail.com>
2023-11-27 21:21:01 +0330

    change middleware functionality

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,20 +2,20 @@ import { Context } from '@nuxt/types'
 import Cookies from 'js-cookie'
 export default (ctx: Context) => {
   console.log('[MIDDLEWARE]: AUTH')
-  let token: string = ''
+  let token: string | undefined
 
   if (ctx.req) {
-    const tokenCookie =
-      ctx.req.headers.cookie ??
-      ''.split(';').find((c: string) => c.trim().startsWith('Authorization='))
+    const tokenCookie = ctx.req.headers.cookie
+      ?.split(';')
+      .find((c: string) => c.trim().startsWith('Authorization='))
     token = tokenCookie?.split('=')[1] ?? ''
   } else {
-    token = Cookies.get('Authorization') ?? ''
-
   }
+  token = Cookies.get('Authorization')
+
   ctx.store.commit('setToken', token)
 
-  if (!token && token === '') {
+  if (!token) {
     ctx.redirect('/auth')
     console.log(!ctx.store.getters.getToken)
     console.log('ridirected from auth middleware')
