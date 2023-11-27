a3d34d4a2b58bd8db7a1f65121d35c116ce7ba46
Sahand Karimkhani <sahandkarimkhani@gmail.com>
2023-11-27 23:41:59 +0330

    add error handling to Api/repo

--- Api/repository.ts
+++ Api/repository.ts
@@ -1,58 +1,98 @@
 // **NEW API**
 import VueRouter from 'vue-router'
-import {Route} from 'vue-router'
+import { Route } from 'vue-router'
 import axios from 'axios'
 import Cookies from 'js-cookie'
 
-
 export default class Api {
   private token = Cookies.get('Authorization')
-  
+
   constructor(private resource: string) {}
-  post<T>(payload: T) {
-    return axios.post(`http://localhost:3000/${this.resource}`, payload, {
-      headers: {
-        Authorization: `Bearer ${this.token}`,
-      },
-    })
+  async post<T>(payload: T) {
+    try {
+      const res = await axios.post(
+        `http://localhost:3000/${this.resource}`,
+        payload,
+        {
+          headers: {
+            Authorization: `Bearer ${this.token}`,
+          },
+        }
+      )
+      return res
+    } catch (error: any) {
+      if (error.response.status === 401) {
+        window.$nuxt.$router.push('/auth')
+      }
+    }
   }
-  get() {
-    return axios.get(`http://localhost:3000/${this.resource}`, {
-      headers: {
-        Authorization: `Bearer ${this.token}`,
-      },
-    })
-  }
-  put<T>(payload: T) {
-    return axios.put(`http://localhost:3000/${this.resource}`, payload, {
-      headers: {
-        Authorization: `Bearer ${this.token}`,
-      },
-    })
-  }
-  delete() {
-    return axios.delete(`http://localhost:3000/${this.resource}`, {
-      headers: {
-        Authorization: `Bearer ${this.token}`,
-      },
-    })
-  }
-  patch<T>(payload: T) {
-    return axios.patch(
-      `http://localhost:3000/${this.resource}`,
-      payload,
-      {
+  async get() {
+    try {
+      const res = await axios.get(`http://localhost:3000/${this.resource}`, {
         headers: {
           Authorization: `Bearer ${this.token}`,
         },
+      })
+      return res
+    } catch (error: any) {
+      if (error.response.status === 401) {
+        window.$nuxt.$router.push('/auth')
       }
-    )
+    }
+  }
+  async put<T>(payload: T) {
+    try {
+      const res = await axios.put(
+        `http://localhost:3000/${this.resource}`,
+        payload,
+        {
+          headers: {
+            Authorization: `Bearer ${this.token}`,
+          },
+        }
+      )
+      return res
+    } catch (error: any) {
+      if (error.response.status === 401) {
+        window.$nuxt.$router.push('/auth')
+      }
+    }
+  }
+  async delete() {
+    try {
+      const res = await axios.delete(`http://localhost:3000/${this.resource}`, {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      })
+      return res
+    } catch (error: any) {
+      if (error.response.status === 401) {
+        window.$nuxt.$router.push('/auth')
+      }
+    }
+  }
+  async patch<T>(payload: T) {
+    try {
+      const res = await axios.patch(
+        `http://localhost:3000/${this.resource}`,
+        payload,
+        {
+          headers: {
+            Authorization: `Bearer ${this.token}`,
+          },
+        }
+      )
+      return res
+    } catch (error: any) {
+      window.$nuxt.$router.push('/auth')
+    }
   }
 }
 
 declare module 'vue/types/vue' {
   interface Vue {
-    $router: VueRouter,
+    $router: VueRouter
     $route: Route
   }
 }
